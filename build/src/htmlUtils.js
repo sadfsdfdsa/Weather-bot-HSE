8e118fe4ebc07f3a7de1343458e4803af8f9632b
Artem Shuvaev <shuvaevlol@gmail.com>
2022-12-21 14:24:11 +0500

    feat: daily pressure + windspeed

--- build/src/htmlUtils.js
+++
@@ -1,5 +0,0 @@
-export const getStrong = (text) => `<strong>${text}</strong>`;
-export const getItalic = (text) => `<i>${text}</i>`;
-export const getUnderline = (text) => `<u>${text}</u>`;
-export const getLine = () => `<br/>`;
-//# sourceMappingURL=htmlUtils.js.map
--- build/src/store.js
+++
@@ -1,2 +0,0 @@
-export const userLocationById = {};
-//# sourceMappingURL=store.js.map
